f18e2f764bf48bd5bd772b6f328a305698f9db2b
jmelberg <jordan.melberg@okta.com>
2018-10-23 20:29:01 -0700

    Remove html-proffer

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ gem 'github-pages'
 gem 'jekyll-assets', '2.3.2'
 gem 'jekyll-redirect-from'
 gem 'jekyll-include-cache'
-gem 'html-proofer', '3.7.2'
 gem 'tzinfo-data', '1.2017.2'
 gem 'wdm', '~> 0.1.0' if Gem.win_platform?
 
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -16,18 +16,5 @@ fold npm_markdown_lint npm run markdown-lint
 # Build site and Run tests
 fold npm_test npm test
 
-# Run External Link Check ONLY on Travis Cron Jobs
-if [ "$TRAVIS_EVENT_TYPE" == "cron" ];
-then
-  if ! bundle exec ./scripts/htmlproofer.rb;
-  then
-    echo "Failed HTML link validation."
-    exit 1;
-  else
-    echo -e "\xE2\x9C\x94 No broken internal/external links!"
-    exit 0;
-  fi
-fi
-
 # Run /dist lint checker
 fold npm_post_build_lint npm run post-build-lint

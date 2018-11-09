0a5e24014b23de8d9002d7fb6c8167a272998fd1
Brian Retterer <bretterer@gmail.com>
2018-11-09 10:35:50 -0500

    404 page (#2469)

--- docs/.vuepress/vuepress-theme-default/layouts/404.vue
+++ docs/.vuepress/vuepress-theme-default/layouts/404.vue
@@ -1,26 +1,48 @@
 <template>
   <div class="theme-container">
-    <div class="content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
+
+    <TopNavigation/>
+
+    <PromoBanner/>
+
+    <div class="Body error_404 content custom">
+      <div class="region region-content">
+        <div class="Hero--empty is-active"></div>
+        <div class="Body-sections">
+          <section class="block block-bean">
+            <section class="Section--developer-404 Section--basicContent ViewMode--default Background--0 u-align--left u-padding--medium">
+              <div class="Wrap">
+                <div class="Section-content  ">
+                  <h1 class="Hero-title">404 Page Not Found</h1>
+                  <p>The web page or file you were trying to reach cannot be found. It may have been moved to a
+                    different location or removed because the information it contained is no longer valid.</p>
+                  <p>Common errors that can result in broken links include:</p>
+                  <p>- Truncated URL's.&nbsp;Some email applications may break a URL due to line breaks.&nbsp;You may
+                    need to cut and paste a URL from your email application into your browser's location field.</p>
+                  <p>- Case Sensitivity.&nbsp;URL's are case sensitive.&nbsp;"File.html" and "file.html" are not
+                    considered the same file by the server.&nbsp;Please verify that the URL is entered correctly as
+                    printed.</p>
+                  <p>Please <a href="mailto:info@okta.com">email</a> your feedback/comments.</p>
+                </div>
+              </div>
+              <div class="Background-curve"></div>
+            </section>
+          </section>
+        </div>
+      </div>
     </div>
+
+    <Footer/>
+
   </div>
 </template>
 
 <script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
 
 export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
+  components: {
+    TopNavigation: () => import('../components/TopNavigation.vue'),
+    Footer: () => import('../components/Footer.vue')
   }
 }
 </script>

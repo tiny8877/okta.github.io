68024093eb79a7bebfed322ca7fda5a030f4bf4f
Brian Retterer <bretterer@gmail.com>
2018-12-26 18:44:18 -0500

    Update scripts.sh for allowing bacon to function

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# fix for bacon to be able to function.
+YARN_REGISTRY=https://registry.yarnpkg.com
+OKTA_REGISTRY=${ARTIFACTORY_URL}/api/npm/npm-okta-master
+
+# Yarn does not utilize the npmrc/yarnrc registry configuration
+# if a lockfile is present. This results in `yarn install` problems
+# for private registries. Until yarn@2.0.0 is released, this is our current
+# workaround.
+#
+# Related issues:
+#  - https://github.com/yarnpkg/yarn/issues/5892
+#  - https://github.com/yarnpkg/yarn/issues/3330
+
+# Replace yarn artifactory with Okta's
+sed -i "s#${YARN_REGISTRY}#${OKTA_REGISTRY}#" yarn.lock
 
 # Where the generated VuePress site will be placed
 GENERATED_SITE_LOCATION="../packages/docs/.vuepress/dist"
@@ -22,4 +37,13 @@ export PATH="${PATH}:$(yarn global bin)"
 # Install required dependencies
 yarn global add @okta/ci-update-package
 yarn global add @okta/ci-pkginfo
+
+
+if ! yarn install ; then
+  echo "yarn install failed! Exiting..."
+  exit ${FAILED_SETUP}
+fi
+
+# Revert the original change
+sed -i "s#${OKTA_REGISTRY}#${YARN_REGISTRY}#" yarn.lock
 

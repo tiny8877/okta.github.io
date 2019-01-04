2c5800b491a62627ac6980b5e555f741b5aeda17
Brian Retterer <bretterer@gmail.com>
2019-01-04 16:39:00 -0500

    [VuePress] Update resource markdown (#2626)
    * Updating ApiOperations and css flags for api-resources
    
    * Updates line.replace
    
    * Remove new line
    
    * WIP
    
    * Creates a new component for creating Category Links
    
    * Removes new line from rule

---
+++ packages/@okta/vuepress-theme-default/global-components/CategoryLinks.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul>
+    <li v-for="link in links" :key="link.path">
+      <router-link :to="link.path">{{ link.title }}</router-link>
+      <br v-if="link.frontmatter.excerpt && showExcerpt">
+      <span class="description" v-if="link.frontmatter.excerpt && showExcerpt">{{ link.frontmatter.excerpt }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "CategoryLinks",
+    props: {
+      "category": {
+        type: String
+      },
+      "where_exp": {
+        type: String
+      },
+      "sort": {
+        type: String
+      },
+      "showExcerpt": {
+        type: Boolean,
+        default: true
+      }
+    },
+    computed: {
+      links() {
+        let links = this.$site.pages.filter((el) => el.frontmatter.category === this.category);
+
+        if ( this.where_exp != undefined ) {
+           let toRemove = links.filter((el) => el.frontmatter[this.where_exp] && el.frontmatter[this.where_exp] == true);
+           links = links.filter((el) => toRemove.indexOf(el) < 0);
+        }
+
+        if ( this.sort ) {
+          let ascDesc = this.sortAsc ? -1 : 1;
+          return links.sort((a, b) => ascDesc * a[this.sort].localeCompare(b.title));
+        }
+
+        return links
+      }
+    }
+  }
+</script>
